064e0e00b81b539eac0efa411447db7f8a3da6d0
adamprancz <adam.prancz@gmail.com>
2018-07-06 13:34:43 +0200

    Add upload aliases

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -5,3 +5,5 @@ Yii::setAlias('@api', dirname(dirname(__DIR__)).'/api');
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)).'/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)).'/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)).'/console');
+Yii::setAlias('@images', dirname(dirname(__DIR__)).'/uploads');
+Yii::setAlias('@uploads', dirname(dirname(__DIR__)).'/uploads');
